95557d7c112f7d821d1042c2e2bcdd86f8c64948
Vincent Batts <vbatts@hashbangbash.com>
2018-01-10 17:01:25 -0500

    examples: succesful connect to the service
    Now for something fruitful
    
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- examples/one.py
+++ examples/one.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python
 
+import grpc
 import kubernetes.cri.api_pb2_grpc as api
 
 print(api)
+
+channel = grpc.insecure_channel('/var/run/crio/crio.sock')
+image_stub = api.ImageServiceStub(channel)
+runtime_stub = api.RuntimeServiceStub(channel)
+
+print(image_stub)
+print(runtime_stub)
