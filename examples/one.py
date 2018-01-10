352c5cbb67965a334942014b73eeb6427f3b3065
Vincent Batts <vbatts@hashbangbash.com>
2018-01-10 13:38:58 -0500

    *: distutils
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

---
+++ examples/one.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+
+import grpc
+import api_pb2_grpc
+
+print(api_pb2_grpc)
---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='kubernetes-cri',
+      version='0.1',
+      description='python bindings for the Kubernetes Container Runtime Interface (CRI) gRPC',
+      author='Vincent Batts',
+      author_email='vbatts@hashbangbash.com',
+      url='https://github.com/vbatts/k8s-cri',
+      py_modules=[
+          'api_pb2_grpc',
+          'api_pb2',
+          'github',
+          'github.com',
+          'github.com.gogo',
+          'github.com.gogo.protobuf',
+          'github.com.gogo.protobuf.gogoproto',
+          'github.com.gogo.protobuf.gogoproto.gogo_pb2',
+          ],
+        )
