590edf7acb7d87cd9b1328bcb012938e6d3cf4d6
Vincent Batts <vbatts@hashbangbash.com>
2018-01-11 11:41:59 -0500

    examples: now it shows images and containers
    Signed-off-by: Vincent Batts <vbatts@hashbangbash.com>

--- examples/one.py
+++ examples/one.py
@@ -11,3 +11,18 @@ runtime_stub = api.RuntimeServiceStub(channel)
 
 vers = runtime_stub.Version(pb.VersionRequest())
 print(vers)
+
+list_images_response = image_stub.ListImages(pb.ListImagesRequest())
+print("found {} images".format(len(list_images_response.images)))
+for image in list_images_response.images:
+    print("id: {}".format(image.id))
+    print("size: {}".format(image.size))
+    print("uid: {}".format(image.uid))
+    print("username: {}".format(image.username))
+    print("repo_tags: {}".format(image.repo_tags))
+    print("repo_digests: {}\n".format(image.repo_digests))
+
+list_containers_response = runtime_stub.ListContainers(pb.ListContainersRequest())
+print("found {} containers".format(len(list_containers_response.containers)))
+for ctr in list_containers_response.containers:
+    print(ctr)
